2f98389b6f8425813842966778daecdafa068048
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-10-04 18:57:21 +0200

    chore: fix benchmarks

--- benchmarks/send.js
+++ benchmarks/send.js
@@ -3,7 +3,6 @@ const pull = require('pull-stream')
 const pair = require('pull-pair/duplex')
 const PeerId = require('peer-id')
 const crypto = require('libp2p-crypto')
-const pushable = require('pull-pushable')
 
 const secio = require('../src')
 
@@ -12,9 +11,13 @@ const ids = []
 
 suite.add('createKey', function (d) {
   crypto.generateKeyPair('RSA', 2048, (err, key) => {
-    const id = PeerId.createFromPrivKey(key.bytes)
-    ids.push(id)
-    d.resolve()
+    PeerId.createFromPrivKey(key.bytes, (err, id) => {
+      if (err) {
+        throw err
+      }
+      ids.push(id)
+      d.resolve()
+    })
   })
 }, {
   defer: true
@@ -23,9 +26,13 @@ suite.add('createKey', function (d) {
   const p = pair()
 
   createSession(p[0], (err, local) => {
-    if (err) throw err
+    if (err) {
+      throw err
+    }
     createSession(p[1], (err, remote) => {
-      if (err) throw err
+      if (err) {
+        throw err
+      }
       sendMessages(local, remote)
     })
   })
@@ -62,9 +69,15 @@ suite.add('createKey', function (d) {
 
 function createSession (insecure, cb) {
   crypto.generateKeyPair('RSA', 2048, (err, key) => {
-    if (err) return cb(err)
-    const id = PeerId.createFromPrivKey(key.bytes)
+    if (err) {
+      return cb(err)
+    }
+    PeerId.createFromPrivKey(key.bytes, (err, id) => {
+      if (err) {
+        return cb(err)
+      }
 
-    cb(null, secio.encrypt(id, key, insecure))
+      cb(null, secio.encrypt(id, key, insecure))
+    })
   })
 }
