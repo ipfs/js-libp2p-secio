185f66f54ee4d2e0f0dbe0149e67952e996d6e68
Maciej Krüger <mkg20001@gmail.com>
2019-10-21 14:17:31 +0200

    refactor: entry file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const pull = require('pull-stream/pull')
-const Connection = require('interface-connection').Connection
 const assert = require('assert')
 const PeerInfo = require('peer-info')
 const debug = require('debug')
@@ -11,40 +9,46 @@ log.error = debug('libp2p:secio:error')
 const handshake = require('./handshake')
 const State = require('./state')
 
-module.exports = {
-  tag: '/secio/1.0.0',
-  encrypt (localId, conn, remoteId) {
-    assert(localId, 'no local private key provided')
-    assert(conn, 'no connection for the handshake  provided')
+async function secure (localPeer, duplex, remotePeer) {
+  /* assert(localId, 'no local private key provided')
+  assert(conn, 'no connection for the handshake  provided')
 
-    const timeout = 60 * 1000 * 5
-    const state = new State(localId, remoteId, timeout)
+  const timeout = 60 * 1000 * 5
+  const state = new State(localId, remoteId, timeout)
 
-    const { handler, awaitConnected } = handshake(state)
+  const { handler, awaitConnected } = handshake(state)
 
-    const encryptedConnection = new Connection(undefined, conn)
-    encryptedConnection.awaitConnected = (async () => { // NOTE: all errors this throws should ideally be also sent down the wire of the connection
-      await awaitConnected
+  const encryptedConnection = new Connection(undefined, conn)
+  encryptedConnection.awaitConnected = (async () => { // NOTE: all errors this throws should ideally be also sent down the wire of the connection
+    await awaitConnected
 
-      await new Promise((resolve, reject) => { // TODO: promisify
-        conn.getPeerInfo((err, peerInfo) => {
-          encryptedConnection.setInnerConn(new Connection(state.secure, conn))
+    await new Promise((resolve, reject) => { // TODO: promisify
+      conn.getPeerInfo((err, peerInfo) => {
+        encryptedConnection.setInnerConn(new Connection(state.secure, conn))
 
-          if (err) { // no peerInfo yet, means I'm the receiver
-            encryptedConnection.setPeerInfo(new PeerInfo(state.id.remote))
-          }
+        if (err) { // no peerInfo yet, means I'm the receiver
+          encryptedConnection.setPeerInfo(new PeerInfo(state.id.remote))
+        }
 
-          resolve()
-        })
+        resolve()
       })
-    })()
+    })
+  })()
 
-    pull(
-      conn,
-      handler,
-      conn
-    )
+  pull(
+    conn,
+    handler,
+    conn
+  )
 
-    return encryptedConnection
-  }
+  return encryptedConnection */
+}
+
+module.exports = {
+  protocol: '/secio/1.0.0',
+
+  // since SECIO is symetric, we only need one function here
+  secureInbound: secure,
+  secureOutbound: secure
+
 }
