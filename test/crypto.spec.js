d9473743757d7330b4661461c1d8b7025aa8052d
Maciej Krüger <mkg20001@gmail.com>
2019-10-24 10:32:48 +0200

    fix: tests

--- test/crypto.spec.js
+++ test/crypto.spec.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const tests = require('libp2p-interfaces/src/crypto/tests')
-const yourCrypto = require('./your-crypto')
+const SECIO = require('..')
+
 tests({
   setup () {
     // Set up your crypto if needed, then return it
-    return yourCrypto
+    return SECIO
   },
   teardown () {
     // Clean up your crypto if needed
