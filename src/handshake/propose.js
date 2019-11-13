55a74247bd2ade5c375d7ada82b7ed085863c95a
Maciej Krüger <mkg20001@gmail.com>
2019-11-13 21:29:34 +0100

    refactor: apply changes from review

--- src/handshake/propose.js
+++ src/handshake/propose.js
@@ -16,9 +16,9 @@ module.exports = async function propose (state, wrapped) {
 
   log('1. propose - reading proposal')
   const msg = (await wrapped.readLP()).slice()
-  log('1. propose - read proposal', msg.slice())
+  log('1. propose - read proposal', msg)
 
-  await crypto.identify(state, msg.slice())
+  await crypto.identify(state, msg)
   await crypto.selectProtocols(state)
 
   log('1. propose - finish')
